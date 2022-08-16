bda0200ae08346348f8258139c170da25c62da9b
Makhti <cherkesov_makhti@mail.ru>
2022-08-16 22:02:48 +0300

    add If_ElseExample005_IfElse

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "eldar")
+{
+    Console.WriteLine("Yes, it is Eldar!!!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
